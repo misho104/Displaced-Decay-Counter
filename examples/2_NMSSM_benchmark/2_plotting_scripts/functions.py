21f5487d624ad3f19fc38e64adafe82907e8dea8
Sho Iwamoto <iwamoto@g-mail.nsysu.edu.tw>
2024-03-24 15:19:18 +0800

    refactor example plotter

--- examples/2_NMSSM_benchmark/2_plotting_scripts/functions.py
+++ examples/2_NMSSM_benchmark/2_plotting_scripts/functions.py
@@ -1,170 +1,132 @@
 #!/usr/bin/env python
-import os
 import matplotlib as mpl  # Plotting module
-
-mpl.use(
-    "Agg"
-)  # Setting Matplotlib backend as non-interactive, necessary for use in Linus
 import matplotlib.pyplot as plt  # Plotting module
 
+default_font = {
+    "family": "serif",
+    "style": "normal",
+    "variant": "normal",
+    "weight": "light",
+    "size": 16,
+}
 
-def detector_styles(
-    detector,
-):  # Plotstyles for different detectors. This includes color in hexadecimal code, linestyle, and legend-label using LaTeX
-    if detector == "ANUBIS0":  # blue
-        return "#005AC8", "--", r"ANUBIS0, 3 ab$^{-1}$"
-    if detector == "ANUBIS1":  # blue
-        return "#005AC8", "-", r"ANUBIS, 3 ab$^{-1}$"
-    elif detector == "AL3X":  # pink
-        return "#00A0FA", "-", r"AL3X, 250 fb$^{-1}$"
-    elif detector == "CODEXB0":  # orange
-        return "#AA0A3C", "--", r"CODEX-b0, 300 fb$^{-1}$"
-    elif detector == "CODEXB1":  # orange
-        return "#AA0A3C", "-", r"CODEX-b, 300 fb$^{-1}$"
-    elif detector == "FACET":  # brown
-        return "#000000", "-", r"FACET, 3 ab$^{-1}$"
-    elif detector == "FASER":  # green
-        return "#FA78FA", "-", r"FASER, 150 fb$^{-1}$"
-    elif detector == "FASER2":  # green
-        return "#ff0505", "-", r"FASER2, 3 ab$^{-1}$"
-    elif detector == "MAPP1":  # red
-        return "#0A9B4B", "-", r"MAPP1, 30 fb$^{-1}$"
-    elif detector == "MAPP2":  # red
-        return "#FF825F", "-", r"MAPP2, 300 fb$^{-1}$"
-    elif detector == "MATHUSLA0":  # yellow
-        return "#a39103", "--", r"MATHUSLA0, 3 ab$^{-1}$"
-    elif detector == "MATHUSLA1":  # yellow
-        return "#a39103", "-", r"MATHUSLA, 3 ab$^{-1}$"
-    elif detector == "MATHUSLA2":  # yellow
-        return "#a39103", "-.", r"MATHUSLA2, 3 ab$^{-1}$"
-    return "", "", ""
+_detector_styles = {
+    "ANUBIS0": ("#005AC8", "--", r"ANUBIS0, 3 ab$^{-1}$"),  # blue
+    "ANUBIS1": ("#005AC8", "-", r"ANUBIS, 3 ab$^{-1}$"),  # blue
+    "AL3X": ("#00A0FA", "-", r"AL3X, 250 fb$^{-1}$"),  # pink
+    "CODEXB0": ("#AA0A3C", "--", r"CODEX-b0, 300 fb$^{-1}$"),  # orange
+    "CODEXB1": ("#AA0A3C", "-", r"CODEX-b, 300 fb$^{-1}$"),  # orange
+    "FACET": ("#000000", "-", r"FACET, 3 ab$^{-1}$"),  # brown
+    "FASER": ("#FA78FA", "-", r"FASER, 150 fb$^{-1}$"),  # green
+    "FASER2": ("#ff0505", "-", r"FASER2, 3 ab$^{-1}$"),  # green
+    "MAPP1": ("#0A9B4B", "-", r"MAPP1, 30 fb$^{-1}$"),  # red
+    "MAPP2": ("#FF825F", "-", r"MAPP2, 300 fb$^{-1}$"),  # red
+    "MATHUSLA0": ("#a39103", "--", r"MATHUSLA0, 3 ab$^{-1}$"),  # yellow
+    "MATHUSLA1": ("#a39103", "-", r"MATHUSLA, 3 ab$^{-1}$"),  # yellow
+    "MATHUSLA2": ("#a39103", "-.", r"MATHUSLA2, 3 ab$^{-1}$"),  # yellow
+}
 
 
-def load_data(
-    benchmark, benchmarkscenario=""
-):  # Reading data files. Returns a dictionary containing all the important information
-    # Setup return dictionary
-    data = dict()
-    parent_directory = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-    data["plotpath"] = os.path.join(parent_directory, "3_plots")
-    data["datapath"] = os.path.join(
-        parent_directory, "3_plots", "data_points" + benchmarkscenario
-    )
-    data["detectors"] = list()
-    # Read data for every detector
-    for filename in os.listdir(data["datapath"]):
-        filepath = os.path.join(data["datapath"], filename)
-        print("\tReading" + filepath)
+def detector_styles(detector):
+    # Returns plot styles for detectors: hex color,  line style, and LaTeX legend-label
+    color, linestyle, label = _detector_styles.get(detector, ("", "", ""))
+    return {"color": color, "linestyle": linestyle, "label": label}
 
-        ctaus = list()
-        yValues = list()
-        if os.stat(filepath).st_size == 0:  # Skip it, if it is empty
+
+def load_data(data_dir):
+    data = {}
+    if not data_dir.is_dir():
+        raise RuntimeError("data_dir is not directory", data_dir)
+    for child in data_dir.iterdir():
+        if not child.is_file():
             continue
-        if filepath.split(".")[-1] != "dat":  # Skip it, if it is the wrong file
+        if not child.suffix == ".dat":  # Skip it, if it is the wrong file
             continue
-        with open(
-            filepath, "r"
-        ) as f:  # Read file, but skip detectors with very low sensitivity for certain benchmarks
-            cur_det = filename.split(".")[0]
-            if benchmark == "two" and cur_det == "FASER":
+        detector_name = child.stem
+        print(f"\tReading {child}\t> {detector_name}")
+
+        content_text = child.read_text()
+        content = {"ctau": [], "value": []}
+        for line in content_text.splitlines():
+            tokens = line.split("\t")
+            if len(tokens) == 1:
                 continue
-            if benchmark == "three" and (cur_det == "FASER" or cur_det == "FASER2"):
-                continue
-            data["detectors"].append(cur_det)
-            for line in f:
-                tokens = line.split("\t")
-                if (
-                    len(tokens) == 1
-                ):  # If the split-line is empty, continue to next line.
-                    continue
-                ctaus.append(float(tokens[0]))
-                yValues.append(float(tokens[1]))
-            data[cur_det] = dict()
-            data[cur_det]["ctau"] = ctaus
-            data[cur_det]["yVal"] = yValues
-            (
-                data[cur_det]["color"],
-                data[cur_det]["linestyle"],
-                data[cur_det]["label"],
-            ) = detector_styles(cur_det)
+            content["ctau"].append(float(tokens[0]))
+            content["value"].append(float(tokens[1]))
+        # skip if the file is empty
+        if len(content["ctau"]) == 0:
+            continue
+        content.update(detector_styles(detector_name))
+        data[detector_name] = content
     return data
 
 
-def plot_make_up(
-    ax, fontdict="", benchmark="", mstring=""
-):  # Creating a grid, labels, etc.
-    if fontdict == "":
-        fontdict = {
-            "family": "serif",
-            "style": "normal",
-            "variant": "normal",
-            "weight": "light",
-            "size": 16,
-        }
+def draw_plot(data_dict, plot_style, font=default_font):
+    # Setting regarding the typewriting of axis and label
+    plt.rc("text", usetex=True)
+    plt.rc("font", **font)
+    mpl.rc("font", **font)
 
-    ax.set_xscale("log")  # Set log-scale of x-axis
-    locmaj = mpl.ticker.LogLocator(
-        base=10.0,
-        subs=(
-            0.1,
-            1.0,
-        ),
-        numticks=100,
-    )
-    ax.xaxis.set_major_locator(locmaj)
-    locmin = mpl.ticker.LogLocator(
-        base=10.0, subs=(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1), numticks=100
-    )
-    ax.xaxis.set_minor_locator(locmin)
+    # Setting plots
+    fig = plt.figure(num=None, figsize=(5, 4), dpi=300, facecolor="w", edgecolor="k")
+    ax = fig.add_subplot(1, 1, 1)
+
+    # Plotting each detector
+    for name in sorted(data_dict.keys()):
+        print("Plotting " + name)
+        value = data_dict[name]
+        plt.loglog(
+            value["ctau"],
+            value["value"],
+            color=value["color"],
+            linestyle=value["linestyle"],
+            linewidth=1,
+            label=value["label"],
+        )
+
+    # make up and filename
+    plot_make_up(ax, plot_style)
+
+
+def plot_make_up(ax, plot_style):
+    print(f"decorating with plot style {plot_style}")
+    major = mpl.ticker.LogLocator(subs=(0.1, 1.0), numticks=100)
+    minor = mpl.ticker.LogLocator(subs=[i / 10 for i in range(1, 10)], numticks=100)
+    ax.set_xscale("log")
+    ax.xaxis.set_major_locator(major)
+    ax.xaxis.set_minor_locator(minor)
     ax.xaxis.set_minor_formatter(mpl.ticker.NullFormatter())
 
-    ax.set_yscale("log")  # Set log-scale of y-axis
-    locmaj = mpl.ticker.LogLocator(
-        base=10.0,
-        subs=(
-            0.1,
-            1.0,
-        ),
-        numticks=100,
-    )
-    ax.yaxis.set_major_locator(locmaj)
-    locmin = mpl.ticker.LogLocator(
-        base=10.0, subs=(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1), numticks=100
-    )
-    ax.yaxis.set_minor_locator(locmin)
+    major = mpl.ticker.LogLocator(subs=(0.1, 1.0), numticks=100)
+    minor = mpl.ticker.LogLocator(subs=[i / 10 for i in range(1, 10)], numticks=100)
+    ax.set_yscale("log")
+    ax.yaxis.set_major_locator(major)
+    ax.yaxis.set_minor_locator(minor)
     ax.yaxis.set_minor_formatter(mpl.ticker.NullFormatter())
 
     # Labels
-    if benchmark == "one":
+    if plot_style == "one":
         plt.xlabel(r"c$\tau_{N}$ [m]")
         plt.ylabel(r"Br$(B^+/B^0 \to e^+/\bar{\nu}_e   N)$")
         plt.title(r"$m_N=1$~GeV")
         plt.axis([1e-2, 1e7, 1e-13, 1e-5])
         plt.legend(loc="lower right", prop={"size": 7})
-    if benchmark == "two":
+    elif plot_style in ["twoA", "twoB"]:
+        if plot_style == "twoA":
+            bound_y = 4.08 / 0.06
+            title = r"$M_{A}=410$~GeV,\,\,$M_{A_S}=70$~GeV"
+        else:
+            bound_y = 1.26 / 0.06 / 0.66
+            title = r"$M_{A}=500$~GeV,\,\,$M_{A_S}=200$~GeV"
         plt.xlabel(r"c$\tau_{A_S}$ [m]")
-        plt.ylabel(
-            r"$\sigma (pp\rightarrow A\rightarrow A_S\,h_{\mathrm{SM}})$ [fb]"
-        )  # ($ggF\rightarrow A\rightarrow h+A_s$)
-        if mstring == "A":
-            plt.axhline(
-                (4.08 / 0.06), 1e-5, 1e10, color="black", linestyle="--", linewidth=1.75
-            )
-            plt.title(r"$M_{A}=410$~GeV,\,\,$M_{A_S}=70$~GeV")
-        if mstring == "B":
-            plt.axhline(
-                (1.26 / 0.06 / 0.66),
-                1e-5,
-                1e10,
-                color="black",
-                linestyle="--",
-                linewidth=1.75,
-            )
-            plt.title(r"$M_{A}=500$~GeV,\,\,$M_{A_S}=200$~GeV")
+        plt.ylabel(r"$\sigma (pp\rightarrow A\rightarrow A_S\,h_{\mathrm{SM}})$ [fb]")
+        # ($ggF\rightarrow A\rightarrow h+A_s$)
+        plt.axhline(bound_y, color="black", linestyle="--", linewidth=1.75)
+        plt.title(title)
         plt.axis([1e-2, 1e7, 1e-1, 1e7])
         plt.legend(loc="lower right", prop={"size": 7})
-    if benchmark == "three":
-        plt.axhline(3.0, 1e-5, 1e10, color="black", linestyle="--", linewidth=2.25)
+    elif plot_style == "three":
+        plt.axhline(3.0, color="black", linestyle="--", linewidth=2.25)
         plt.xlabel(r"c$\tau_{\tilde{\chi}^0_1}$ [m]")
         plt.ylabel(r"Signal Events")  # ($ggF\rightarrow A\rightarrow h+A_s$)
         # plt.ylabel(r'Br($\tilde{l}_L \rightarrow e^+\tilde{\chi}^0_1$)')
--- examples/3_pair_slepton_benchmark/2_plotting_scripts/plot_benchmark_three.py
+++ examples/3_pair_slepton_benchmark/2_plotting_scripts/plot_benchmark_three.py
@@ -1,48 +1,26 @@
-print("Loading Libraries")
-import sys, os  # Basic functions
-import matplotlib as mpl  # Plotting module
+#!env python
 
-mpl.use(
-    "Agg"
-)  # Setting Matplotlib backend as non-interactive, necessary for use in Linus
-import matplotlib.pyplot as plt  # Plotting module
+import os
+import pathlib
+import sys  # Basic functions
+
 import functions as func
+import matplotlib as mpl  # Plotting module
+import matplotlib.pyplot as plt  # Plotting module
 
-#######################################################################################
-# Reading Data
-print("Loading Input")
-data_dict = func.load_data("three")
-#######################################################################################
-# Setting regarding the typewriting of axis and label
-plt.rc("text", usetex=True)
-font = {
-    "family": "serif",
-    "style": "normal",
-    "variant": "normal",
-    "weight": "light",
-    "size": 16,
-}
-plt.rc("font", **font)
-mpl.rc("font", **font)
+if __name__ == "__main__":
+    # Setting Matplotlib backend as non-interactive, necessary for use in Linus
+    mpl.use("Agg")
 
-# Setting plots
-fig = plt.figure(num=None, figsize=(5, 4), dpi=300, facecolor="w", edgecolor="k")
-ax = fig.add_subplot(1, 1, 1)
-#######################################################################################
-# Plotting each detector
-for det in data_dict["detectors"]:
-    print("Plotting " + det)
-    plt.loglog(
-        data_dict[det]["ctau"],
-        data_dict[det]["yVal"],
-        color=data_dict[det]["color"],
-        linestyle=data_dict[det]["linestyle"],
-        linewidth=1,
-        label=data_dict[det]["label"],
-    )
-#######################################################################################
-# Creating a grid, labels, etc., and saving plot
-func.plot_make_up(ax, fontdict=font, benchmark="three", mstring="")
-os.system('if not exist "' + data_dict["plotpath"] + '" mkdir ' + data_dict["plotpath"])
-savepath = os.path.join(data_dict["plotpath"], "benchmark_three.pdf")
-plt.savefig(savepath, format="pdf", bbox_inches="tight", dpi=200)
+    data_dir = pathlib.Path("../3_plots/data_points/")
+    plot_file = pathlib.Path("../3_plots/benchmark_three.pdf")
+    plot_style = "three"
+
+    data_dict = func.load_data(data_dir)
+    # skip detectors with very low sensitivity for certain benchmarks
+    data_dict.pop("FASER", None)
+    data_dict.pop("FASER2", None)
+
+    func.draw_plot(data_dict, plot_style)
+    plot_file.parent.mkdir(parents=True, exist_ok=True)
+    plt.savefig(plot_file, format="pdf", bbox_inches="tight", dpi=200)
